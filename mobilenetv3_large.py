12a7403b12abc16929ab5af66702d2de3fe0ae6f
xujialu <xujialu801516@163.com>
2019-05-19 15:11:58 +0800

    changed

--- mobilenetv3_large.py
+++ mobilenetv3_large.py
@@ -1,88 +1,133 @@
-import numpy as np
 import tensorflow as tf
 import keras
 from keras import Input,Model
-from layers import Bneck, HSwish, SEModule
+from keras.layers import Activation
+from layers import Bneck
 import os
+from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
 """MobileNetv3 in keras
 """
+from keras.utils.generic_utils import get_custom_objects
+
+
+def Hswish(x):
+    return x * tf.nn.relu6(x + 3) / 6
+
+
+get_custom_objects().update({'custom_activation': Activation(Hswish)})
 
 def MobileNetv3_large(num_classes=1000,input_shape=(224,224,3)):
     x = Input(shape=input_shape)
     out = keras.layers.Conv2D(16,3,strides=2,padding='same',use_bias=False)(x)
     out = keras.layers.BatchNormalization()(out)
-    out = HSwish()(out)
+    out = Activation(Hswish)(out)
 
-    out = Bneck(3,16,16,16,keras.layers.ReLU(),1,None)(out)
-    out = Bneck(3,16,64,24,keras.layers.ReLU(),2,None)(out)
-    out = Bneck(3,24,72,24,keras.layers.ReLU(),1,None)(out)
-    out = Bneck(5,24,72,40,keras.layers.ReLU(),2,SEModule(72))(out)
-    out = Bneck(5,40,120,40,keras.layers.ReLU(),1,SEModule(120))(out)
-    out = Bneck(5,40,120,40,keras.layers.ReLU(),1,SEModule(120))(out)
-    out = Bneck(3,40,240,80,HSwish(),2,None)(out)
-    out = Bneck(3,80,200,80,HSwish(),1,None)(out)
-    out = Bneck(3,80,184,80,HSwish(),1,None)(out)
-    out = Bneck(3,80,184,80,HSwish(),1,None)(out)
-    out = Bneck(3,80,480,112,HSwish(),1,SEModule(480))(out)
-    out = Bneck(3,112,672,112,HSwish(),1,SEModule(672))(out)
-    out = Bneck(5,112,672,160,HSwish(),2,SEModule(672))(out)
-    out = Bneck(5,160,960,160,HSwish(),1,SEModule(960))(out)
-    out = Bneck(5,160,960,160,HSwish(),1,SEModule(960))(out)
+    out = Bneck(out,3,16,16,16,keras.layers.ReLU(),1,False)
+    out = Bneck(out,3,16,64,24,keras.layers.ReLU(),2,False)
+    out = Bneck(out,3,24,72,24,keras.layers.ReLU(),1,False)
+    out = Bneck(out,5,24,72,40,keras.layers.ReLU(),2,True)
+    out = Bneck(out,5,40,120,40,keras.layers.ReLU(),1,True)
+    out = Bneck(out,5,40,120,40,keras.layers.ReLU(),1,True)
+    out = Bneck(out,3,40,240,80,Activation(Hswish),2,False)
+    out = Bneck(out,3,80,200,80,Activation(Hswish),1,False)
+    out = Bneck(out,3,80,184,80,Activation(Hswish),1,False)
+    out = Bneck(out,3,80,184,80,Activation(Hswish),1,False)
+    out = Bneck(out,3,80,480,112,Activation(Hswish),1,True)
+    out = Bneck(out,3,112,672,112,Activation(Hswish),1,True)
+    out = Bneck(out,5,112,672,160,Activation(Hswish),1,True)
+    out = Bneck(out,5,160,672,160,Activation(Hswish),2,True)
+    out = Bneck(out,5,160,960,160,Activation(Hswish),1,True)
 
-    out = keras.layers.Conv2D(filters=960,kernel_size=1,strides=1,use_bias=False)(out)
+    out = keras.layers.Conv2D(filters=960,kernel_size=3,strides=1,padding='same')(out)
     out = keras.layers.BatchNormalization()(out)
-    out = HSwish()(out)
+    out = Activation(Hswish)(out)
     out = keras.layers.GlobalAveragePooling2D()(out)
     out = keras.layers.Reshape((1,1,-1))(out)
     out = keras.layers.Conv2D(filters=1280,kernel_size=1,strides=1)(out)
-    out = HSwish()(out)
-    out = keras.layers.Conv2D(filters=num_classes,kernel_size=1,strides=1,activation='softmax')(out)
+    out = Activation(Hswish)(out)
+    out = keras.layers.Conv2D(filters=num_classes,kernel_size=1,strides=1)(out)
     out = keras.layers.Flatten()(out)
+    out = keras.layers.Softmax()(out)
     model = Model(inputs=x,outputs=out)
     return model
 
+def generate(batch, shape, ptrain, pval):
+    """Data generation and augmentation
+
+    # Arguments
+        batch: Integer, batch size.
+        size: Integer, image size.
+        ptrain: train dir.
+        pval: eval dir.
+
+    # Returns
+        train_generator: train set generator
+        validation_generator: validation set generator
+        count1: Integer, number of train set.
+        count2: Integer, number of test set.
+    """
+
+    #  Using the data Augmentation in traning data
+    datagen1 = ImageDataGenerator(
+        rescale=1. / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        rotation_range=90,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        horizontal_flip=True)
+
+    datagen2 = ImageDataGenerator(rescale=1. / 255)
+
+    train_generator = datagen1.flow_from_directory(
+        ptrain,
+        target_size=shape,
+        batch_size=batch,
+        class_mode='categorical')
+
+    validation_generator = datagen2.flow_from_directory(
+        pval,
+        target_size=shape,
+        class_mode='categorical')
+
+    count1 = 0
+    for root, dirs, files in os.walk(ptrain):
+        for each in files:
+            count1 += 1
+
+    count2 = 0
+    for root, dirs, files in os.walk(pval):
+        for each in files:
+            count2 += 1
+
+    return train_generator, validation_generator, count1, count2
 
 def train():
-    batch_size = 32
-    target_size = (224,224)
-    data_dir = "E:\\study\\dataset\\mini_flowers"
-    train_dir = os.path.join(data_dir,"train")
-    val_dir = os.path.join(data_dir,"val")
-    img_gen = ImageDataGenerator(rotation_range=20,
-                                 width_shift_range=0.2,
-                                 height_shift_range=0.2,
-                                 horizontal_flip=True,
-                                 rescale=1/255.,
-                                 zoom_range=0.5)
-    train_generator = img_gen.flow_from_directory(directory=train_dir,
-                                                  batch_size=batch_size,
-                                                  shuffle=True,
-                                                  target_size=target_size,
-                                                  class_mode='categorical')
-    val_generator = img_gen.flow_from_directory(directory=val_dir,
-                                                batch_size=batch_size,
-                                                target_size=target_size,
-                                                class_mode='categorical')
     model = MobileNetv3_large(5)
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=keras.optimizers.Adam(lr=0.001),
-                  metrics=['acc'])
+    save_dir = "./snapshot"
+    shape = (224, 224, 3)
+    n_class = 5
+    batch = 32
+    train_dir = "E:\\NN\\dataset\\mini_flowers\\train"
+    val_dir = "E:\\NN\\dataset\\mini_flowers\\val"
 
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
 
-    snapshots = "./snapshots"
-    if not os.path.exists(snapshots):
-        os.mkdir(snapshots)
-    callback = tf.keras.callbacks.ModelCheckpoint(filepath=os.path.join(snapshots,"mobilenetv3_{{epoch:02d}}.h5".format()),
-                                                  verbose=1,
-                                                  period=10,
-                                                  monitor='val_loss')
-    model.fit_generator(train_generator,steps_per_epoch=int(1000/batch_size),
-                        epochs=50,validation_data=val_generator,
-                        validation_steps=int(500/batch_size),
-                        callbacks=[callback]
-                                 )
+    opt = Adam(lr=0.001)
 
+    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
 
-if __name__ == '__main__':
+    train_generator, validation_generator, count1, count2 = generate(batch, shape[:2], train_dir, val_dir)
+
+    hist = model.fit_generator(
+        train_generator,
+        validation_data=validation_generator,
+        steps_per_epoch=count1 // batch,
+        validation_steps=count2 // batch,
+        epochs=50,
+    )
+
+if __name__ == "__main__":
     train()
